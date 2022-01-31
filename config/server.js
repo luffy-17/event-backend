2e8877148b0cf26f8a2625d2ad1807a5fac664c5
nikunjpadasala <nikunjpadasala@gmail.com>
2022-01-31 18:32:28 +0500

    app keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["c4YiWIQ0C0GL8egTbQI2XQ==","1GCg2JAZf6lyVnZhEVzpCw==","MEuhOw+zjC0QWOBx8ho/hw==","cSZGMZ8xzYM3eh1jUTM4Rg=="]
   },
 });
